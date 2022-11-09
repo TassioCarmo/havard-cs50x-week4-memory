c396cdc0cd23db8fbcc39ee5b3cd889c9127220b
Tassio Luiz D. Carmo <31789624+TassioCarmo@users.noreply.github.com>
2022-11-09 13:03:18 -0300

    Update volume.c

--- lab-4/volume/volume.c
+++ lab-4/volume/volume.c
@@ -7,6 +7,7 @@
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
+
 int main(int argc, char *argv[])
 {
     // Check command-line arguments
@@ -33,22 +34,39 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
+
     // TODO: Copy header from input file to output file
+
+    // Creates a place in the memory to read the header of the size 44
     uint8_t header[HEADER_SIZE];
 
-    fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, output);
+    //size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+    /* ptr, which is the address (of the first byte) of memory into which to read the data,
+    *  size, which is the size (in bytes) of the type of data to read,
+    *  nmemb, which is the number of those types to read at once, and
+    *  stream, which is the pointer to a FILE returned by fopen.
+    */
+
+    //Reads headersize bytes reads into the header
+    fread(header, sizeof(header), 1, input);
+    //writes the content of the header into the output file
+    fwrite(header, sizeof(header), 1, output);
+
 
     // TODO: Read samples from input file and write updated data to output file
+    //16-bit integer
     int16_t buffer;
+
+    //read from the input file into the buffer passing the address of the buffer, reading a sample of 16bits(2bytes)
+    //Repeatily reading a sample until reaches the end of the file returning 0 when it ends
     while (fread(&buffer, sizeof(buffer), 1, input))
     {
-        //change volume
+        //multiples
         buffer *= factor;
         fwrite(&buffer, sizeof(buffer), 1, output);
     }
-
     // Close files
     fclose(input);
     fclose(output);
 }
+
